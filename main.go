92e9c9a18bba5cf8c6c80e0527c79a010c48b560
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-31 21:46:35 +0100

    somekind of registration

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/mail"
 )
+
+
+const minUserNameLength int = 3
 
 type Repository interface {
 	Get()
@@ -15,34 +19,40 @@ type Repository interface {
 }
 
 type UserRegisterRequest struct {
-	username        string
-	email           string
-	password        string
-	passwordConfirm string
+	Username        string `json:"username"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	PasswordConfirm string `json:"passwordConfirm"`
 }
 
-func (r *UserRegisterRequest) validate() []error {
-	var errors []error
-	if userRegister.password != userRegister.passwordConfirm {
-		errors = append(errors, ApiError{
-			status: http.StatusBadRequest,
-			msg:    "Password doesn't match confirmed password",
-		})
+func (r *UserRegisterRequest) validate() map[string]string {
+	var errors = map[string]string{}
+	if r.Password != r.PasswordConfirm {
+		errors["password"] = "password and password confirm doesn't match"
 	}
-	return nil
-	// check for username uniqnes
-	// check for email uniqnes
+	if len(r.Username) < minUserNameLength {
+		errors["username"] = "username minimum length equals 3"
+	}
+	_, err :=  mail.ParseAddress(r.Email)
+	if err != nil {
+		errors["email"] = "invalid email"
+	}
+	// if username is not unique
+	errors["username"] = fmt.Sprintf("%s and username is not unique", errors["username"])
+	errors["email"] = fmt.Sprintf("%s and email is not unique", errors["email"])
+
+	return errors
 
 }
 
 // ctrl + d | ctrl + u scrolling
 type ApiError struct {
 	Status int    `json:"status"`
-	Msg    string `json:"msg"`
+	Msg    interface{} `json:"msg"`
 }
 
 func (e ApiError) Error() string {
-	return fmt.Sprintf("error: %s status code: %d\n", e.msg, e.status)
+	return fmt.Sprintf("error: %s status code: %d\n", e.Msg, e.Status)
 }
 
 func main() {
@@ -54,7 +64,7 @@ func main() {
 
 	router.Handle("/foo", rh)
 	// mux.Handle("/register", http.HandlerFunc(handleRegister))
-	router.HandleFunc("GET /register", handler(handleRegister))
+	router.HandleFunc("POST /register", handler(handleRegister))
 
 	log.Printf("Listening on %s\n", listenAddr)
 
@@ -63,16 +73,19 @@ func main() {
 
 func handleRegister(w http.ResponseWriter, r *http.Request) error {
 	request := new(UserRegisterRequest)
+	defer r.Body.Close()
 	if err := json.NewDecoder(r.Body).Decode(request); err != nil {
 		return ApiError{
 			Status: http.StatusBadRequest,
 			Msg:    "Wrong register payload",
 		}
 	}
-	if errors := request.validate(); errors != nil {
+	log.Printf("register user request: %+v error", request)
+	if errors := request.validate(); len(errors) > 0 {
+		fmt.Printf("errors occured: %+v\n", errors)
 		return ApiError{
 			Status: http.StatusUnprocessableEntity,
-			Msg:    "",
+			Msg:    errors,
 		}
 	}
 	return nil
@@ -84,8 +97,9 @@ func handler(fn apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := fn(w, r); err != nil {
 			if e, ok := err.(ApiError); ok {
-				// write json with error
-				fmt.Println(e)
+				w.WriteHeader(e.Status)
+				json.NewEncoder(w).Encode(e)
+
 			}
 		}
 	}
