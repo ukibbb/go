0971430b683844f1d2acf199ed0b34eb712f8487
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-05 20:47:17 +0100

    register modal done

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 // useEffect(() => {
 //     // useEffect it will run at least once when component mounts
 //     // code we want to run
@@ -13,6 +14,7 @@
 
 // CSS
 
+import { Button } from "@/components/ui/button";
 import { RegisterForm } from "./features/register";
 import {
     Card,
@@ -21,31 +23,72 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Sun, Moon } from "lucide-react";
+import { useTheme } from "next-themes";
 
+export function ModeToggle() {
+    const { setTheme } = useTheme();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Welcome to Ukasz App!</CardTitle>
-                    <CardDescription>
-                        This app is about nothing.
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <Tabs defaultValue="login" className="w-[400px]">
-                        <TabsList>
-                            <TabsTrigger value="register">Register</TabsTrigger>
-                            <TabsTrigger value="password">Login</TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="login"></TabsContent>
-                        <TabsContent value="register">
-                            <RegisterForm />
-                        </TabsContent>
-                    </Tabs>
-                </CardContent>
-            </Card>
-        </div>
+        <>
+            <div className="p-3">{ModeToggle()}</div>
+            <div className="flex justify-center items-center h-screen">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Welcome to Ukasz App!</CardTitle>
+                        <CardDescription>
+                            This app is about nothing.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Tabs defaultValue="login" className="w-[400px]">
+                            <TabsList>
+                                <TabsTrigger value="register">
+                                    Register
+                                </TabsTrigger>
+                                <TabsTrigger value="password">
+                                    Login
+                                </TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="login"></TabsContent>
+                            <TabsContent value="register">
+                                <RegisterForm />
+                            </TabsContent>
+                        </Tabs>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     );
 }
