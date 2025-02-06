34cbc3d86591854505635b69eda44acff7d1bcca
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-06 21:13:15 +0100

    data

--- data.go
+++ data.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 )
+
+type Storable interface {
+	GetKeyForRedis() string
+}
 
 type User struct {
 	Id        uuid.UUID
@@ -16,4 +21,14 @@ type User struct {
 	Role      string
 }
 
-type Entity = User
+func (u User) GetKeyForRedis() string {
+	return fmt.Sprintf("user:%s", u.Id)
+}
+
+type Order struct {
+	Id uuid.UUID
+}
+
+func (o Order) GetKeyForRedis() string {
+	return fmt.Sprintf("order:%s", o.Id)
+}
