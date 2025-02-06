0937810df59d2ad89a5bb2318228b71c95e0b222
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-06 21:14:22 +0100

    tests

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-func main() {
-	s := NewServer(":3000")
+import (
+	"fmt"
 
-	go s.Start()
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	// s := NewServer(":3000")
+
+	// go s.Start()
+	//
+	ds := NewRedisDataStore[User](&redis.Options{
+		Addr:     "localhost:5000",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	u, err := ds.Create(User{
+		Id:       uuid.New(),
+		Username: "ukasz",
+		Email:    "ukasz.bulinski.com",
+	})
+	fmt.Println(u, "user", err, "error")
 
 	select {}
 
