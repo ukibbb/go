433d74c51ecc654c3ade23dd01253e6f7f4507f5
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-11 20:27:40 +0100

    fixing in memory datastroe

--- in_memory_datastore.go
+++ in_memory_datastore.go
@@ -7,16 +7,16 @@ import (
 )
 
 type InMemoryDataStore[T Storable] struct {
-	storage map[uuid.UUID]*T
+	storage map[uuid.UUID]T
 }
 
 func NewInMemoryDataStore[T Storable]() *InMemoryDataStore[T] {
 	return &InMemoryDataStore[T]{
-		storage: make(map[uuid.UUID]*T),
+		storage: make(map[uuid.UUID]T),
 	}
 }
 
-func (db *InMemoryDataStore[T]) Get(id uuid.UUID) (e *T, err error) {
+func (db *InMemoryDataStore[T]) Get(id uuid.UUID) (e T, err error) {
 	e, ok := db.storage[id]
 	if !ok {
 		return e, fmt.Errorf("error: entity %s not found in Storage", id)
@@ -24,15 +24,19 @@ func (db *InMemoryDataStore[T]) Get(id uuid.UUID) (e *T, err error) {
 	return e, err
 
 }
-func (db *InMemoryDataStore[T]) Create(e *T) (*T, error) {
+
+func (db *InMemoryDataStore[T]) GetAll() (t T, err error) {
+	return t, err
+}
+func (db *InMemoryDataStore[T]) Create(e T) (T, error) {
 	uuid := uuid.New()
 	db.storage[uuid] = e
 	return e, nil
 }
 
-func (db *InMemoryDataStore[T]) Update(e *T) (*T, error) {
+func (db *InMemoryDataStore[T]) Update(e T) (T, error) {
 	return e, nil
 }
-func (db *InMemoryDataStore[T]) Delete(id uuid.UUID) (e *T, err error) {
+func (db *InMemoryDataStore[T]) Delete(id uuid.UUID) (e T, err error) {
 	return e, err
 }
