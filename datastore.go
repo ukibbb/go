2741a0b6cb456e846b912e4e0a1ed0a2de994580
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-15 15:02:30 +0100

    datstore

--- datastore.go
+++ datastore.go
@@ -1,14 +1,10 @@
 package main
 
-import (
-	"github.com/google/uuid"
-)
-
 // Contains actual logic needed to retrive data from storage.
 type DataStore[T Storable] interface {
-	Get(id uuid.UUID) (T, error)
-	GetAll() (T, error)
+	Get(id string) (T, error)
+	GetAll() ([]T, error)
 	Create(T) (T, error)
 	Update(T) (T, error)
-	Delete(id uuid.UUID) (T, error)
+	Delete(id string) (T, error)
 }
