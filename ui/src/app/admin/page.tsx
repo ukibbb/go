1b1b8320ed97f834021fd221517acad873ebed63
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-16 21:47:42 +0100

    admin

--- ui/src/app/admin/page.tsx
+++ ui/src/app/admin/page.tsx
@@ -1,5 +1,14 @@
-import Users from "./users";
+import { UsersTable } from "./users";
+
+export type User = {
+    id: string;
+    email: string;
+    username: string;
+    isActive: boolean;
+    role: string;
+};
 export default async function Admin() {
-    //const response = await fetch("http://localhost:8000/v1/users");
-    return <Users />;
+    const response = await fetch("http://localhost:8000/v1/users");
+    const users: { status: number; msg: User[] } = await response.json();
+    return <UsersTable users={users.msg} />;
 }
