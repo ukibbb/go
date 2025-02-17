4b5fe072a8f9bc67ba7b6694075cca2e9a87508d
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-17 15:30:53 +0100

    data

--- data.go
+++ data.go
@@ -21,6 +21,15 @@ type User struct {
 	Role      string `json:"role" redis:"role"`
 }
 
+type UserResponse struct {
+	Id        string `json:"id" redis:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"createdAt"`
+	IsActive  bool   `json:"isActive"`
+	Role      string `json:"role"`
+}
+
 func (u User) GetKeyForRedis() string {
 	return fmt.Sprintf("%s:%s", u.Namespace(), u.Id)
 }
