7bc7cbada0e2c758e70b6cf09c87d021f979da6f
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-02-23 16:25:11 +0100

    publisher

--- publisher.go
+++ publisher.go
@@ -6,15 +6,19 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+func NewRedisClient(opts *redis.Options) *redis.Client {
+	return redis.NewClient(opts)
+}
+
 func main() {
 	c :=
-		redis.NewClient(&redis.Options{
+		NewRedisClient(&redis.Options{
 			Addr:     "localhost:5000",
 			Password: "", // no password set
 			DB:       0,  // use default DB
 		})
 	ctx := context.Background()
-	err := rdb.Publish(ctx, "worker").Err()
+	err := c.Publish(ctx, "worker", "hello from publisher").Err()
 	if err != nil {
 		panic(err)
 	}
